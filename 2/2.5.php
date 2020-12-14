39a7ddfbc743dc06d6bb9834c21405097632aef0
Celestialis <nagibatorkir@gmail.com>
2020-12-14 14:31:08 +0700

    2

---
+++ 2/2.5.php
@@ -0,0 +1,16 @@
+<?php
+    $title = 'Title 1';
+    $content = 'Some content';
+    $year = '2020';
+?>
+
+<html>
+  <body>
+    <div class='content'>
+    <h1><?= $content?></h1>
+    <p><?= $title?></p>
+    <p><?= $year?></p>
+    </div>
+  <footer>
+  &copy; <?php echo date('Y'); ?> Все права защищены.
+  </footer>
